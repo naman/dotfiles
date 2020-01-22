77a09e49843bd84143b724bfe3c342a1a8e36762
naman <namanspace@gmail.com>
2020-01-22 20:35:14 +0530

    Going back to fish-shell. zsh so slow!

--- setup.sh
+++ setup.sh
@@ -3,20 +3,23 @@
 cd 
 
 # # setup fish config
-brew cask install fish-shell
+# brew cask install fish-shell
+sudo apt install fish
+
 
 ln -s -f ~/dotfiles/config.fish ~/.config/fish/config.fish 
 ln -s -f ~/dotfiles/functions ~/.config/fish/functions
 curl -L https://get.oh-my.fish | fish
 omf update
+
+omf install bang-bang sudope
 omf doctor
 omf reload
 
-setup
-
 # # setup tmux config
-brew install tmux
-brew cask install tmux
+# brew install tmux
+# brew cask install tmux
+sudo apt install tmux
 
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f ~/.tmux/.tmux.conf
